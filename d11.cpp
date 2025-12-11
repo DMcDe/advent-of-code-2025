826267da7fdcc25b9cad330b60871c74f0089b8e
DMcDe <dmcde@umich.edu>
2025-12-11 16:38:57 -0500

    upload day 11 (not working on part B)

---
+++ d11.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <sstream>
+#include <unordered_set>
+#include <unordered_map>
+
+int numPaths(std::unordered_set<std::string> &vis, std::unordered_map<std::string, std::unordered_set<std::string>> &adj, std::string cur, int cnt, bool partB) {
+    if (cur == "out") {
+        for (auto s : vis) std::cout << s << " ";
+        std::cout << "\n";
+
+        if (partB && (!vis.contains("fft") || !vis.contains("dac"))) return cnt;
+        return 1 + cnt;
+    }
+    for (auto s : adj[cur]) {
+        if (!vis.contains(s)) {
+            vis.insert(s);
+        cnt = numPaths(vis, adj, s, cnt, partB);
+            vis.erase(s);
+        }
+    }
+    return cnt;
+}
+
+int partA(std::unordered_map<std::string, std::unordered_set<std::string>> &adj) {
+    int res = 0;
+    std::unordered_set<std::string> vis;
+    vis.insert("you");
+    res = numPaths(vis, adj, "you", 0, false);
+    
+    return res;
+}
+
+int partB(std::unordered_map<std::string, std::unordered_set<std::string>> &adj) {
+    int res = 0;
+    std::unordered_set<std::string> vis;
+    vis.insert("svr");
+    res = numPaths(vis, adj, "svr", 0, true);
+
+    return res;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> adj;
+
+    std::string str;
+    std::cout << "Test\n";
+
+    while (std::getline(std::cin, str)) {
+        std::stringstream ss(str);
+
+        std::string src, dst;
+        ss >> src;
+        src.pop_back();
+
+        while (ss >> dst) {
+            adj[src].insert(dst);
+        }
+    }
+
+    std::cout << "Part A solution: " << partA(adj) << "\n";
+    std::cout << "Part B solution: " << partB(adj) << "\n";
+
+    return 0;
+}
