5a360ace3d1766b1b80837b98e480649bc213c41
DMcDe <dmcde@umich.edu>
2025-12-11 18:19:59 -0500

    add memoization to day 11

--- d11.cpp
+++ d11.cpp
@@ -3,20 +3,24 @@
 #include <unordered_set>
 #include <unordered_map>
 
-int numPaths(std::unordered_set<std::string> &vis, std::unordered_map<std::string, std::unordered_set<std::string>> &adj, std::string cur, int cnt, bool partB) {
+int numPaths(std::unordered_set<std::string> &vis, std::unordered_map<std::string, std::unordered_set<std::string>> &adj, 
+    std::unordered_map<std::string, int> &memo, std::string cur, int cnt, bool partB) {
     if (cur == "out") {
         for (auto s : vis) std::cout << s << " ";
         std::cout << "\n";
 
-        if (partB && (!vis.contains("fft") || !vis.contains("dac"))) return cnt;
+        // if (partB && (!vis.contains("fft") || !vis.contains("dac"))) return cnt;
         return 1 + cnt;
     }
+    if (memo.contains(cur)) return cnt + memo[cur];
     for (auto s : adj[cur]) {
+        int before = cnt;
         if (!vis.contains(s)) {
             vis.insert(s);
-        cnt = numPaths(vis, adj, s, cnt, partB);
+            cnt = numPaths(vis, adj, memo, s, cnt, partB);
             vis.erase(s);
         }
+        memo[s] = cnt - before;
     }
     return cnt;
 }
@@ -24,8 +28,9 @@ int numPaths(std::unordered_set<std::string> &vis, std::unordered_map<std::strin
 int partA(std::unordered_map<std::string, std::unordered_set<std::string>> &adj) {
     int res = 0;
     std::unordered_set<std::string> vis;
+    std::unordered_map<std::string, int> memo;
     vis.insert("you");
-    res = numPaths(vis, adj, "you", 0, false);
+    res = numPaths(vis, adj, memo, "you", 0, false);
     
     return res;
 }
@@ -33,8 +38,9 @@ int partA(std::unordered_map<std::string, std::unordered_set<std::string>> &adj)
 int partB(std::unordered_map<std::string, std::unordered_set<std::string>> &adj) {
     int res = 0;
     std::unordered_set<std::string> vis;
+    std::unordered_map<std::string, int> memo;
     vis.insert("svr");
-    res = numPaths(vis, adj, "svr", 0, true);
+    res = numPaths(vis, adj, memo, "svr", 0, true);
 
     return res;
 }
